848f2fd815403683ec65f7dd4cf063f27bdb93ce
tran phong <thanhphong190@gmail.com>
2017-11-17 07:58:14 +0700

    Upload

---
+++ sinhvien.java
@@ -0,0 +1,18 @@
+import java.lang.*;
+import java.io.*;
+import java.util.Date;
+class SVien 
+{
+	private  String hoTen;
+	private Date ngaySinh;
+	private double dtb;
+	public SVien(String ht,Date ns,double d)
+	{
+		hoTen= ht;
+		ngaySinh=ns;
+		dtb=d;
+	}
+	public String layHoten()
+	{
+		return hoTen;
+	}
